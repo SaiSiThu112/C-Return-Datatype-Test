992fa171e37c91e9226f2341a88429d222a2e24b
SaiSiThu112 <linenghan97211@gmail.com>
2023-01-15 15:48:02 +0630

    initial commit

---
+++ Distance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Distance
+    {
+        public int feet { get; set; }
+        public int inches { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Test
+{
+	class Program
+	{
+        static dynamic Sum(int totalInches)
+        {
+            int feet = totalInches / 12;
+            int inches = totalInches % 12;
+
+            /*for one time use object and can return any datatype*/
+            dynamic output = new ExpandoObject();
+            output.feet = feet;
+            output.inches = inches;
+            return output;
+        }
+        
+        static void Main(string[] arguments)
+		{
+            dynamic output = Sum(20);
+            Console.WriteLine("This is total value of feet --> " + output.feet);
+            Console.WriteLine("This is total value of inches --> " + output.inches);
+            Distance d = Calculate(128);
+            Console.WriteLine("Total value of feet --> "+ d.feet + " and Total value of inches -- >" +  d.inches);
+        }
+
+        static Distance Calculate(int totalValue)
+        {
+            Distance distance = new Distance();
+            distance.feet = totalValue / 12;
+            distance.inches = totalValue % 12;
+            return distance;
+        }
+    }
+}
+
